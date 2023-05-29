49474d2c50b1b93b02eba744da198d3cbd87a536
mariaCubides <mcubides1@udi.edu.co>
2023-05-29 17:00:55 -0500

    docs: :tada: Priimeros pasos PHP seccion 5
    Estructura basica de un script PHP y primero hola mundo

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+/*
+TODO: 5. INTRODUCCION A PHP
+*Se realiza la estructura BASICA de un script PHP
+*Se realiza las carpetas manejadas
+?Estructura Basica Proyecto en Web/PHP
+*Se analiza la estructura basica de php
+*Se revisa la ejecucion del servidor temporal sin embargo nosotros no lo realizamos.
+*Se realiza nuestro primer hola mundo :D
+
+echo '<h1> Hola Mundo </h1>';
+*/
+
+/* 
+$texto="Mundo mafe 2023";
+$mensaje=sprintf("Hola %s",$texto);
+echo $mensaje;
+*define("MENSAJE","mundo"); arma la cadena
+define("MENSAJE","mundo");
+printf("%s %s",$texto, MENSAJE);
+var_dump($texto);
+*/
+
+/*
+*variable numerica
+*/
+
+$edad=20;
+
+/*
+*declarar la variable de texto con USO ESTRICTO 
+*codigo ESTRICTO
+*/
+
+declare(stric_types=1);
+/*----------------------- */
+$nombre= (string)"Mafe";
+
+/*
+TODO: 9 numeros y operadores
+*/
+$numero1=20;
+$numero2=10;
+$numero3=50;
+
+var_dump($numero1>$numero2);
+echo "<br>";
+
+?>
+
